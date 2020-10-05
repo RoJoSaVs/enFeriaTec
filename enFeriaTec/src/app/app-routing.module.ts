48391ec93b1a36384a14959d2e129c74420843d2
Calquito <allancalqui@gmail.com>
2020-10-05 12:31:51 -0600

    Style

--- enFeriaTec/src/app/app-routing.module.ts
+++ enFeriaTec/src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { VistaProductorComponent } from './vista-productor/vista-productor.component';
 import { VistaAdministradorComponent } from './vista-administrador/vista-administrador.component';
 import { VistaPublicoComponent } from './vista-publico/vista-publico.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent},
   { path: 'productor', component: VistaProductorComponent},
   { path: 'administrador', component: VistaAdministradorComponent},
-  { path: 'público', component: VistaPublicoComponent},
+  { path: 'publico', component: VistaPublicoComponent,
+    children: [
+      { path: 'form', component: LoginFormComponent, },
+                
+    ]},
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
